68d6f8342ac427e2c665d2e99111a7df1c2bbb19
hagarSamy <hagarsami63@gmail.com>
2023-06-21 02:51:01 -0700

    0putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: printing (_putchar)
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
