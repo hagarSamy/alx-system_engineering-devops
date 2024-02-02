825bb05abe44894b32bb429b5f4ec59ccd607d99
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-03 01:28:29 +0200

    simple match

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 #program executed using Ruby interpreter
+# puts: outputs the string passed\n
+# ARGV = array containing cmnd-line arguments passed to script
+# scan = searches the string for matches (regular expression) returns an array of matches
+# .join = to put them ALL in 1 string
 
-#puts: outputs the string passed\n
-#ARGV = array containing cmnd-line arguments passed to script
-#scan = searches the string for matches (regular expression) returns an array of matches
-#.join = to put them ALL in 1 string
-
-puts ARGV[0].scan(/School/).join
+puts ARGV[0].scan(/\bSchool\b/).join
