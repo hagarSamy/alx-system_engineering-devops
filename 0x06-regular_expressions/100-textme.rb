e39751213111695350e304908dc69d6f1d2e26b0
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-03 02:22:25 +0200

    NOn greedy capturing that stops when finding closing square bracket

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,4 +4,7 @@
 #The ? makes it non-greedy
 #meaning it will stop matching as soon as
 #it finds a closing bracket ]
+#.: any char
+#*: 0 or more of preceding
+#?: 1 or none
 puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
