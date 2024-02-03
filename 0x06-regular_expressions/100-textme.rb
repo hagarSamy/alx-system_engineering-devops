169b622b53142e5d6f3e36d2fbf2a66281b7b4a1
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-03 02:20:03 +0200

    NOn greedy capturing that stops when finding closing square bracket

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+#(.*?): This is a non-greedy capture group that
+#matches any sequence of characters except newline characters.
+#The ? makes it non-greedy
+#meaning it will stop matching as soon as
+#it finds a closing bracket ]
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
