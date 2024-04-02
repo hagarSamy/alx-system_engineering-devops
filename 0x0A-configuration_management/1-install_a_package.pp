3f8b6c79c8910dac079e6966bd77a5d3501f55d4
HagarSamy <hagarsami63@gmai.com>
2024-04-02 15:53:56 +0200

    updates just

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,23 @@
 # Using Puppet to install flask from pip3.
-#!/usr/bin/pup
+# check that python 3 is installed
+package {'python3':
+    ensure   => present,
+}
+
+# Check that pip3 is installed
+package {'python3-pip':
+    ensure => present
+}
+
+# install flask version 2.1.0
 package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+    ensure   =>'2.1.0',
+    name     =>'flask',
+    provider =>'pip'
+}
+
+# install werkzeug
+package {'werkzeug':
+    ensure   =>'2.1.1',
+    provider =>'pip'
 }
