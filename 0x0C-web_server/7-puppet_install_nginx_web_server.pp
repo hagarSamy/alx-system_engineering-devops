c0121dff578279d55b8c39d0aa8632a0dc13bc2f
hagarSamy <hs1800@fayoum.edu.eg>
2024-04-07 14:21:08 +0200

    failed trial

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++
@@ -1,6 +0,0 @@
-# configuring nginx with puppet
-
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i '/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
-}
