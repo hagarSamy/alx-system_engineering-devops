830fb0215fd4bfeb41adece5677412e8ce796a01
HagarSamy <hs1800@fayoum.edu.eg>
2024-05-01 23:03:15 +0300

    installing and configuration using puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # Installs and configure an Nginx server using Puppet instead of Bash
 exec {'install':
   provider => shell,
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo ufw allow 'Nginx HTTP' ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; echo -e "Ceci n'est pas une page\n" | sudo tee /var/www/html/404.html ; sudo sed -i '/listen 80 default_server/a rewrite ^/redirect_me https://github.com/hagarSamy permanent;' /etc/nginx/sites-available/default ; sudo sed -i "/redirect_me/ a\\\terror_page 404 /404.html;" /etc/nginx/sites-available/default ; sudo service nginx restart',
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo ufw allow 'Nginx HTTP' ; echo "Hello World!" | sudo tee /var/www/html/index.html ; echo -e "Ceci n'est pas une page\n" | sudo tee /var/www/html/404.html ; sudo sed -i '/listen 80 default_server/a rewrite ^/redirect_me https://github.com/hagarSamy permanent;' /etc/nginx/sites-available/default ; sudo sed -i "/redirect_me/ a\\\terror_page 404 /404.html;" /etc/nginx/sites-available/default ; sudo service nginx restart',
 }
