b8e674e2deac04c6dc7c9e40313cb3283436de5b
hagarSamy <hs1800@fayoum.edu.eg>
2024-06-04 22:07:06 +0300

    quering reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-# a function that queries the Reddit API and returns the number of subscribers
-# Invalid subreddits may return a redirect to search results.
+"""Module that consumes the Reddit API and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    # r: stands for subredirect in the construct
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    # Set the custom User-Agent to avoid Too Many Requests errors
+    ''' r: stands for subredirect in the construct'''
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    '''Set the custom User-Agent to avoid Too Many Requests errors'''
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-        AppleWebKit/537.36 (KHTML, like Gecko)\
-        Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
