c7e14a89a492add52b134f1113642f1c5e8daa46
hagarSamy <hs1800@fayoum.edu.eg>
2024-06-04 21:54:14 +0300

    quering reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,12 @@ import requests
 def number_of_subscribers(subreddit):
     # r: stands for subredirect in the construct
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    # Set the custom User-Agent to avoid Too Many Requests errors
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
     try:
-        response = requests.get(url, allow_redirects=False)
+        response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
