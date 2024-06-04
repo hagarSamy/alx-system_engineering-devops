6b18a542043c230fdf270deaa058de9a84504d34
hagarSamy <hs1800@fayoum.edu.eg>
2024-06-05 02:18:24 +0300

    recursive function

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 """Module that has a recursive function that
-queries the Reddit API and returns a list containing the
-titles of all hot articles
-for a given subreddit"""
+queries the Reddit APIparses the title of all hot articles,
+and prints a sorted count of
+given keywords (case-insensitive, delimited by spaces"""
 import requests
 
 
-'''after: A parameter used for pagination to fetch the next set of results. '''
-'''after Key Value: A string that is the ID of
-the last post in the current batch of results.'''
-
-
-def recurse(subreddit, hot_list=[], after=None):
-    ''' a function that queries the Reddit API and returns a list containing
-    the titles of all hot articles for a given subreddit'''
-    base_url = "https://www.reddit.com/r"
+def count_words(subreddit, word_list, counts={}, after=None):
+    ''' a function that queries queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit. If no results are
+    found for the given subreddit, the function should return None.'''
+    base_url = "https://www.reddit.com/r/"
     headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
     if after is not None:
-        url = f"{base_url}/{subreddit}/hot.json?after={after}"
+        url = f"{base_url}/r/{subreddit}/hot.json?after={after}"
     else:
-        url = f"{base_url}/{subreddit}/hot.json"
+        url = f"{base_url}/r/{subreddit}/hot.json"
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
