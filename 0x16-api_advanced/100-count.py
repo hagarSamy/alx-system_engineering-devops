42403994c9bec532c1bd3594130085d324f95265
hagarSamy <hs1800@fayoum.edu.eg>
2024-06-05 02:53:33 +0300

    recursive function to sort keywords in all titles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -10,16 +10,19 @@ def count_words(subreddit, word_list, counts={}, after=None):
     ''' a function that queries queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit. If no results are
     found for the given subreddit, the function should return None.'''
-    base_url = "https://www.reddit.com/r/"
+    base_url = "https://www.reddit.com/r"
     headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
     if after is not None:
-        url = f"{base_url}/r/{subreddit}/hot.json?after={after}"
+        url = f"{base_url}/{subreddit}/hot.json?after={after}"
     else:
-        url = f"{base_url}/r/{subreddit}/hot.json"
+        url = f"{base_url}/{subreddit}/hot.json"
+        word_list = [word.lower() for word in word_list]
+        '''count for each word initialized to'''
+        counts = {word: 0 for word in word_list}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
@@ -31,14 +34,28 @@ def count_words(subreddit, word_list, counts={}, after=None):
 
         for post in children:
             ''' data has children key and children has a data key'''
-            hot_list.append(post['data']['title'])
+            hot_list = post['data']['title']
+            split_words = hot_list.split()
+            split_words = [word.lower() for word in split_words]
+            for word in word_list:
+                counts[word] += split_words.count(word)
 
         after = data.get('after')
 
         if after:
             # Recursive call with updated 'after'
-            return recurse(subreddit, hot_list, after)
+            return count_words(subreddit, word_list, counts, after)
         else:
-            return hot_list
+            '''sorts according to values returned from .items -- key and lambda part'''
+            '''.items returns a list of tuples of ks, vs'''
+            '''lambda processes on each element of the iteratable(2nd arg)'''
+            '''reverse=True: This argument sorts the list in descending order.'''
+            sorted_counts = dict(sorted(counts.items(),
+                                        key=lambda item: item[1],
+                                        reverse=True))
+            for key, value in sorted_counts.items():
+                if value > 0:
+                    print(f"{key}: {value}")
+            return counts
     else:
         return None
