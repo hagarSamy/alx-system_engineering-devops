5499c253efb67565c514312163001e45fbfebe30
hagarSamy <hs1800@fayoum.edu.eg>
2024-06-05 20:53:47 +0300

    debugging 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,6 @@
 
 exec {'Fix_php':
     command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    # the search path that Puppet uses to locate the command `sed`
     path    => '/usr/local/bin/:/bin/',
     }
