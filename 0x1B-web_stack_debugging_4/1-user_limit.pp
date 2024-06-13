db1617c669c238cb12b025281e35a1e153bffc22
hagarSamy <hs1800@fayoum.edu.eg>
2024-06-13 22:00:41 +0300

    configuration for user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# This puppet file adds a new configuration for holberton user
+# By changing the number of open file limits, for warning and errors..
+
+file { '/etc/security/limits.conf':
+  ensure  => file,
+  content => "holberton hard nofile 5000\nholberton soft nofile 4000\n",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
